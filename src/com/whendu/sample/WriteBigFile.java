cc711fba71794d489ebb27131dcbc94f6b763735
Maksim Prabarshchuk <Maksim_Prabarshchuk@sap.com>
2015-05-14 17:38:02 +0300

    Read many numbers in buffer

--- src/com/whendu/sample/WriteBigFile.java
+++ src/com/whendu/sample/WriteBigFile.java
@@ -15,14 +15,14 @@ public class WriteBigFile {
 
     public static void main(String[] args) throws IOException {
 
-        final String filePath = "D:\\tmp\\sum\\examples\\100_000_000.txt";
+        final String filePath = "D:\\tmp\\sum\\examples\\1_000_000_000.txt";
 
         double time = System.currentTimeMillis();
 
         final ByteBuffer buffer = ByteBuffer.allocate(4).order(LITTLE_ENDIAN);
         final Path path = Paths.get(filePath);
         try (final SeekableByteChannel channel = Files.newByteChannel(path, CREATE, WRITE)) {
-            for (int i = 1; i <= 100_000_000; i++) {
+            for (int i = 1; i <= 1_000_000_000; i++) {
                 buffer.clear();
                 buffer.putInt(i);
                 buffer.flip();
