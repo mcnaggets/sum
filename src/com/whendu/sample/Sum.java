f094caec332549e416899a40ff1715674d7596c7
Maksim Prabarshchuk <Maksim_Prabarshchuk@sap.com>
2015-05-14 17:38:02 +0300

    Read many numbers in buffer

--- src/com/whendu/sample/Sum.java
+++ src/com/whendu/sample/Sum.java
@@ -7,32 +7,36 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
+import static java.util.stream.Collectors.toList;
 
 public class Sum {
 
     public static final String FILE_PATH = "D:\\tmp\\sum\\examples\\100_000_000.txt";
     public static final Path PATH = Paths.get(FILE_PATH);
+    public static final int THREADS = 4;
+    public static final int BYTES_PER_NUMBER = 4;
+    public static final int NUMBERS_TO_READ = 10000;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
         double time = System.currentTimeMillis();
 
-
         final long size = Files.size(PATH);
-        final ExecutorService executorService = Executors.newFixedThreadPool(4);
-        double sum = executorService.invokeAll(Stream.of(
-                new SumCounter(0, size / 4), new SumCounter(size / 4, size / 2), new SumCounter(size / 2, 3 * size / 4), new SumCounter(3 * size / 4, size)
-//                new SumCounter(0, size / 2), new SumCounter(size / 2, size)
-//                new SumCounter(0, size)
-        ).collect(Collectors.toList())).stream().mapToDouble(Sum::getADouble).sum();
+        final ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
+
+        double sum = executorService.invokeAll(
+                IntStream.range(0, THREADS).mapToObj(i -> new SumCounter(fileOffset(i, size), fileOffset(i + 1, size)))
+                        .collect(toList())).stream().mapToDouble(Sum::getADouble).sum();
         executorService.shutdown();
 
         System.out.printf("Sum %.0f\n", sum);
-        System.out.printf("Elapsed time %s ms", System.currentTimeMillis() - time);
+        System.out.printf("Elapsed time %s ms\n", System.currentTimeMillis() - time);
+    }
+
+    private static long fileOffset(int threadNumber, long fileSize) {
+        return threadNumber * fileSize / THREADS;
     }
 
     private static Double getADouble(Future<Double> t) {
@@ -56,12 +60,14 @@ public class Sum {
         @Override
         public Double call() throws Exception {
             double sum = 0;
-            final ByteBuffer buffer = ByteBuffer.allocate(4);
+            final ByteBuffer buffer = ByteBuffer.allocate(BYTES_PER_NUMBER * NUMBERS_TO_READ).order(LITTLE_ENDIAN);
             try (final SeekableByteChannel channel = Files.newByteChannel(PATH)) {
                 channel.position(from);
                 while (canRead(buffer, channel)) {
                     buffer.flip();
-                    sum += buffer.order(LITTLE_ENDIAN).getInt();
+                    while (buffer.hasRemaining()) {
+                        sum += buffer.getInt();
+                    }
                     buffer.clear();
                 }
             }
